34d56ebaeb5a9ee08a31d80c9c5ff7e1101931cc
İlknur Güzel <ilknrgzl99@gmail.com>
2021-02-24 17:02:53 +0300

    Add project files.

---
+++ oyun/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oyun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 15;
+
+            Console.WriteLine("sayı tahmin etme oyununa hoş geldiniiiiiz.");
+            
+
+            for (int i = 1; i < sayi; i++)
+            {
+                int tahmin = Convert.ToInt32(Console.ReadLine());
+
+                if (tahmin == 25)
+                {
+                    Console.WriteLine("sıcak");
+
+                }
+                else if (tahmin == 20)
+                {
+                    Console.WriteLine("çok sıcaaaak");
+                }
+                else if (tahmin == 30)
+                {
+                    Console.WriteLine("eh işte");
+                }
+                else if (tahmin == 35)
+                {
+                    Console.WriteLine("soğuk");
+                }
+                else if (tahmin > 40) 
+                {
+                    Console.WriteLine("çok soğuk");
+                }
+                else if (tahmin >50)
+                {
+                    Console.WriteLine("çok soğuuuuk");
+                }
+                else if (tahmin == sayi)
+                {
+                    Console.WriteLine("sayıyı buldunuz tebrikler");
+                    break;
+                }
+                
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
